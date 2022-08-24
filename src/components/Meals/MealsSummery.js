b7e16c9fd60c7dc5c0eacd474e2ad26da6e10d23
Fabio Buonocore <fabio.buonocore@infologix.it>
2022-08-24 21:02:32 +0200

    Feat Add Meals List

---
+++ src/components/Meals/MealsSummery.js
@@ -0,0 +1,16 @@
+import classes from './MealsSummery.module.css';
+
+const MealsSummery = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>La vera Pizza Napoletana a casa tua!</h2>
+      <p>Scegli le tue pizze preferite dal menu e goditi la cena a casa.</p>
+      <p>
+        Le nostre pizze rispettano la tradizione della semplicità e ricerca del
+        buono.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummery;
