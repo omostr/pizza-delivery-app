98d5cec268db2b4ed1d0ab87285da44a2c9d6fe2
Fabio Buonocore <fabio.buonocore@infologix.it>
2022-08-24 17:27:17 +0200

    Add HeaderCart Component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,17 @@
-import { Fragment } from "react";
-import headerImage from "../../assets/header.jpg";
-import classes from "./Header.module.css";
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import headerImage from '../../assets/header.jpg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Pizza Delivery</h1>
-        <button> Carrello </button>
+        <HeaderCartButton />
       </header>
-      <div className={classes["main-image"]}>
+      <div className={classes['main-image']}>
         <img src={headerImage} alt="Pizza Napoletana" />
       </div>
     </Fragment>
